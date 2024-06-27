4e6ebf1f3f1e7470d5a4d0179054cf2a1118f5d5
Jay <jaychong1203@gmail.com>
2024-06-27 13:37:02 -0500

    misc

--- ssh-key-submission/src/App.js
+++ ssh-key-submission/src/App.js
@@ -2,26 +2,21 @@ import React, { useState, useEffect } from 'react';
 import SSHKeyForm from './components/SSHKeyForm';
 import SSHKeyList from './components/SSHKeyList';
 import { downloadKeys } from './utils/download';
+import { saveToLocalStorage, loadFromLocalStorage } from './utils/localStorage';
+import './App.css';
 
 const App = () => {
   const [sshKeys, setSshKeys] = useState([]);
 
   useEffect(() => {
-    fetch('https://your-backend-app.herokuapp.com/keys')
-      .then((response) => response.json())
-      .then((data) => setSshKeys(data.map((item) => item.key)));
+    const storedKeys = loadFromLocalStorage();
+    setSshKeys(storedKeys);
   }, []);
 
   const handleFormSubmit = (key) => {
-    fetch('https://your-backend-app.herokuapp.com/keys', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ key }),
-    })
-      .then((response) => response.json())
-      .then((newKey) => setSshKeys([...sshKeys, newKey.key]));
+    const updatedKeys = [...sshKeys, key];
+    setSshKeys(updatedKeys);
+    saveToLocalStorage(updatedKeys);
   };
 
   const handleDownload = () => {
@@ -29,14 +24,14 @@ const App = () => {
   };
 
   return (
-    <div>
-      <h1>SSH Key Submission</h1>
-      <SSHKeyForm onSubmit={handleFormSubmit} />
-      <SSHKeyList sshKeys={sshKeys} />
-      {sshKeys.length > 0 && (
-        <button onClick={handleDownload}>Download Keys</button>
-      )}
-    </div>
+    <div className="centered-content">
+    <h1>SSH Key Submission</h1>
+    <SSHKeyForm onSubmit={handleFormSubmit} />
+    <SSHKeyList sshKeys={sshKeys} />
+    {sshKeys.length > 0 && (
+      <button onClick={handleDownload}>Download Keys</button>
+    )}
+  </div>
   );
 };
 
