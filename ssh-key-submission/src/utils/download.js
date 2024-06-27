05f9dc6ec212fdd76af5ed0289b9445edcca35bd
Jay <jaychong1203@gmail.com>
2024-06-27 13:37:07 -0500

    download format text

--- ssh-key-submission/src/utils/download.js
+++ ssh-key-submission/src/utils/download.js
@@ -1,6 +1,7 @@
 import { saveAs } from 'file-saver';
 
 export const downloadKeys = (keys) => {
-  const blob = new Blob([keys.join('\n')], { type: 'text/plain;charset=utf-8' });
+  const formattedKeys = keys.map(key => `SSH Key: ${key.sshKey}, Username: ${key.username}, Key Type: ${key.KeyType}`).join('\n');
+  const blob = new Blob([formattedKeys], { type: 'text/plain;charset=utf-8' });
   saveAs(blob, 'ssh_keys.txt');
 };
