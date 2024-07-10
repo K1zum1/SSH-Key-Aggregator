1efb510abaeb83ab4a06210312bfb3f1a4c8ce19
Jay <jaychong1203@gmail.com>
2024-07-10 10:42:02 -0500

    MISC

--- ssh-key-submission/api/generateKrl.ts
+++ ssh-key-submission/api/generateKrl.ts
@@ -1,39 +1,39 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-import { Client } from 'pg';
-import { writeFileSync, promises as fsPromises } from 'fs';
-import { join } from 'path';
+// import { VercelRequest, VercelResponse } from '@vercel/node';
+// import { Client } from 'pg';
+// import { writeFileSync, promises as fsPromises } from 'fs';
+// import { join } from 'path';
 
-const client = new Client({
-  connectionString: process.env.POSTGRES_URL,
-});
+// const client = new Client({
+//   connectionString: process.env.POSTGRES_URL,
+// });
 
-client.connect().catch((err) => {
-  console.error('Connection error:', err.stack);
-  process.exit(1);
-});
+// client.connect().catch((err) => {
+//   console.error('Connection error:', err.stack);
+//   process.exit(1);
+// });
 
-export default async function generateKrl(req: VercelRequest, res: VercelResponse) {
-  try {
-    const query = 'SELECT pubKey FROM SSHKeys';
-    const result = await client.query(query);
-    const keys = result.rows.map(row => row.pubKey);
-
-    
-    const krlContent = `# KRL Generated on ${new Date().toISOString()}\n`;
-    const krlKeys = keys.map(key => `revoked-keys: ${key}`).join('\n');
-    const fullKrlContent = krlContent + krlKeys;
+// export default async function generateKrl(req: VercelRequest, res: VercelResponse) {
+//   try {
+//     const query = 'SELECT pubKey FROM SSHKeys';
+//     const result = await client.query(query);
+//     const keys = result.rows.map(row => row.pubKey);
 
     
-    const krlFilePath = join('/tmp', 'revocation-list.krl');
-    writeFileSync(krlFilePath, fullKrlContent);
+//     const krlContent = `# KRL Generated on ${new Date().toISOString()}\n`;
+//     const krlKeys = keys.map(key => `revoked-keys: ${key}`).join('\n');
+//     const fullKrlContent = krlContent + krlKeys;
 
     
-    const krlFile = await fsPromises.readFile(krlFilePath, 'utf-8');
-    res.setHeader('Content-Type', 'application/octet-stream');
-    res.setHeader('Content-Disposition', 'attachment; filename=revocation-list.krl');
-    res.status(200).send(krlFile);
-  } catch (error) {
-    console.error('Error generating KRL file:', error);
-    res.status(500).json({ error: 'Failed to generate KRL file.' });
-  }
-}
+//     const krlFilePath = join('/tmp', 'revocation-list.krl');
+//     writeFileSync(krlFilePath, fullKrlContent);
+
+    
+//     const krlFile = await fsPromises.readFile(krlFilePath, 'utf-8');
+//     res.setHeader('Content-Type', 'application/octet-stream');
+//     res.setHeader('Content-Disposition', 'attachment; filename=revocation-list.krl');
+//     res.status(200).send(krlFile);
+//   } catch (error) {
+//     console.error('Error generating KRL file:', error);
+//     res.status(500).json({ error: 'Failed to generate KRL file.' });
+//   }
+// }
